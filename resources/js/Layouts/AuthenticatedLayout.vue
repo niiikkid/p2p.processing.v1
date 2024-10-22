e82593d43a74942c600a1864728c0519e6bce4e3
Niiikkid <niiikkid@gmail.com>
2024-10-22 11:39:35 +1000

    menu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {usePage, router} from '@inertiajs/vue3';
+import {usePage, router, Link} from '@inertiajs/vue3';
 import {onMounted, ref} from 'vue'
 import { initFlowbite } from 'flowbite'
 import MenuModeSwitcher from "@/Layouts/Partials/MenuModeSwitcher.vue";
@@ -28,6 +28,10 @@ onMounted(() => {
 router.on('success', (event) => {
     initFlowbite()
 })
+
+const openDocs = () => {
+    window.open('/docs', '_blank');
+}
 </script>
 
 <template>
@@ -48,6 +52,18 @@ router.on('success', (event) => {
                     <AdminMenu
                         v-show="is_admin && menuMode === 'admin'"
                     />
+                    <div>
+                        <ul class="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+                            <li>
+                                <Link @click.prevent="openDocs" href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1v6M5 19v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-1M10 3v4a1 1 0 0 1-1 1H5m14 9.006h-.335a1.647 1.647 0 0 1-1.647-1.647v-1.706a1.647 1.647 0 0 1 1.647-1.647L19 12M5 12v5h1.375A1.626 1.626 0 0 0 8 15.375v-1.75A1.626 1.626 0 0 0 6.375 12H5Zm9 1.5v2a1.5 1.5 0 0 1-1.5 1.5v0a1.5 1.5 0 0 1-1.5-1.5v-2a1.5 1.5 0 0 1 1.5-1.5v0a1.5 1.5 0 0 1 1.5 1.5Z"/>
+                                    </svg>
+                                    <span class="ms-3">Документация</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </aside>
 
