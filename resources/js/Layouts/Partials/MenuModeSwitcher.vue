e2263f865e0a0591653812689b813f9d25f98a84
Niiikkid <niiikkid@gmail.com>
2024-10-22 12:16:06 +1000

    fixes

--- resources/js/Layouts/Partials/MenuModeSwitcher.vue
+++ resources/js/Layouts/Partials/MenuModeSwitcher.vue
@@ -13,13 +13,13 @@ const switchMenuMode = (mode) => {
 
 <template>
     <div class="mb-4">
-        <ul class="hidden border border-gray-700 text-sm font-medium text-center text-gray-500 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+        <ul class="hidden border border-gray-200 dark:border-gray-700 text-sm font-medium text-center text-gray-500 rounded-lg sm:flex dark:divide-gray-700 dark:text-gray-400 overflow-hidden">
             <li class="w-full focus-within:z-10">
                 <a
                     @click.prevent="switchMenuMode('trader')"
                     href="#"
-                    class="inline-block w-full p-2 border-r-0 border-gray-200 dark:border-gray-700 rounded-l-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:hover:bg-gray-700"
-                    :class="menuMode === 'trader' ? 'text-gray-900 bg-gray-100 border-r rounded-s-lg dark:bg-gray-700 dark:text-white' : 'bg-white dark:bg-gray-800'"
+                    class="inline-block w-full p-2 border-r-0 border-gray-200 dark:border-gray-700 hover:text-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+                    :class="menuMode === 'trader' ? 'text-gray-900 bg-gray-200 border-r dark:bg-gray-700 dark:text-white' : 'bg-white dark:bg-gray-800'"
                 >
                     Трейдер
                 </a>
@@ -27,8 +27,8 @@ const switchMenuMode = (mode) => {
             <li class="w-full focus-within:z-10">
                 <a @click.prevent="switchMenuMode('admin')"
                    href="#"
-                   class="inline-block w-full p-2 border-s-0 border-gray-200 dark:border-gray-700 rounded-e-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:hover:bg-gray-700"
-                   :class="menuMode === 'admin' ? 'text-gray-900 bg-gray-100 border-r rounded-r-lg dark:bg-gray-700 dark:text-white' : 'bg-white dark:bg-gray-800'"
+                   class="inline-block w-full p-2 border-s-0 border-gray-200 dark:border-gray-700 hover:text-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+                   :class="menuMode === 'admin' ? 'text-gray-900 bg-gray-200 border-r dark:bg-gray-700 dark:text-white' : 'bg-white dark:bg-gray-800'"
                 >
                     Админ
                 </a>
