94ba7312f968cc62db305b0b327d4c6c737ee37a
Niiikkid <niiikkid@gmail.com>
2024-10-22 12:49:54 +1000

    fix

--- resources/js/Components/Table/DeleteAction.vue
+++ resources/js/Components/Table/DeleteAction.vue
@@ -5,7 +5,7 @@ import {Link} from "@inertiajs/vue3";
 <template>
     <button
         @click.prevent
-        class="px-0 py-0 text-gray-900 dark:text-gray-400 dark:hover:text-blue-500 inline-flex items-center hover:underline"
+        class="px-0 py-0 text-red-500 hover:text-red-600 inline-flex items-center hover:underline"
     >
         <svg class="w-[22px] h-[22px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
--- resources/js/Components/Table/ShowAction.vue
+++ resources/js/Components/Table/ShowAction.vue
@@ -10,7 +10,7 @@ import {Link} from "@inertiajs/vue3";
 <template>
     <Link
         :href="link"
-        class="px-0 py-0 text-gray-900 justify-items-center dark:text-gray-400 dark:hover:text-blue-500 inline-flex items-center hover:underline"
+        class="px-0 py-0 justify-items-center text-blue-500 hover:text-blue-600 inline-flex items-center hover:underline"
     >
         <svg class="w-[22px] h-[22px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"/>
