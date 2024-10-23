242801776f9533fbc6484578ec212f96e35648b7
Niiikkid <niiikkid@gmail.com>
2024-10-23 16:27:31 +1000

    light theme

--- resources/js/Pages/Admin/User/Index.vue
+++ resources/js/Pages/Admin/User/Index.vue
@@ -63,7 +63,7 @@ defineOptions({ layout: AuthenticatedLayout })
                             <fwb-table-cell>
                                 <Link
                                     :href="route('admin.users.wallet.index', user.id)"
-                                    class="mr-2 px-0 py-0 text-gray-900 dark:text-gray-400 dark:hover:text-blue-500 inline-flex items-center hover:underline"
+                                    class="mr-2 px-0 py-0 text-yellow-400 hover:text-yellow-500 dark:text-yellow-500 dark:hover:text-yellow-600 inline-flex items-center hover:underline"
                                 >
                                     <svg class="w-[22px] h-[22px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8H5m12 0a1 1 0 0 1 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1Z"/>
