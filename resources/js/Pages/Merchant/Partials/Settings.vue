67daf113eb41919aeb42507ea1da24806c3298d8
Niiikkid <niiikkid@gmail.com>
2024-10-25 16:30:13 +1000

    merchants

--- resources/js/Pages/Merchant/Partials/Settings.vue
+++ resources/js/Pages/Merchant/Partials/Settings.vue
@@ -4,10 +4,13 @@ import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import SaveButton from "@/Components/Form/SaveButton.vue";
 import {useForm} from "@inertiajs/vue3";
+import {ref} from "vue";
 
 const form = useForm({
     callback_url: '',
 });
+
+const commissionEditMode = ref(false)
 </script>
 
 <template>
@@ -92,16 +95,19 @@ const form = useForm({
                             </svg>
                             Комиссия клиента
                         </span>
-                        <button type="button" class="px-2 py-1 text-xs shadow font-medium text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                        <button v-if="commissionEditMode === false" @click.prevent="commissionEditMode = true" type="button" class="px-2 py-1 text-xs shadow font-medium text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
                             Изменить
+                        </button>
+                        <button v-else @click.prevent="commissionEditMode = false" type="button" class="px-2 py-1 text-xs shadow font-medium text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg text-center dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
+                            Сохранить
                         </button>
                     </div>
                 </div>
                 <div class="mb-5">
                     <div>
-                            <span class="bg-white text-xs shadow font-semibold py-1 px-3 border dark:text-gray-200 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-                                RUB
-                            </span>
+                        <span class="bg-white text-xs shadow font-semibold py-1 px-3 border dark:text-gray-200 border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            RUB
+                        </span>
                     </div>
                     <div class="mt-3 grid grid-cols-2 gap-2">
                         <div class="bg-white shadow text-sm font-semibold py-2 px-3 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
@@ -127,7 +133,7 @@ const form = useForm({
                                     9%
                                 </div>
                             </div>
-                            <div class="flex items-center mt-2">
+                            <div v-if="commissionEditMode === true" class="flex items-center mt-2">
                                 <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 12c.263 0 .524-.06.767-.175a2 2 0 0 0 .65-.491c.186-.21.333-.46.433-.734.1-.274.15-.568.15-.864a2.4 2.4 0 0 0 .586 1.591c.375.422.884.659 1.414.659.53 0 1.04-.237 1.414-.659A2.4 2.4 0 0 0 12 9.736a2.4 2.4 0 0 0 .586 1.591c.375.422.884.659 1.414.659.53 0 1.04-.237 1.414-.659A2.4 2.4 0 0 0 16 9.736c0 .295.052.588.152.861s.248.521.434.73a2 2 0 0 0 .649.488 1.809 1.809 0 0 0 1.53 0 2.03 2.03 0 0 0 .65-.488c.185-.209.332-.457.433-.73.1-.273.152-.566.152-.861 0-.974-1.108-3.85-1.618-5.121A.983.983 0 0 0 17.466 4H6.456a.986.986 0 0 0-.93.645C5.045 5.962 4 8.905 4 9.736c.023.59.241 1.148.611 1.567.37.418.865.667 1.389.697Zm0 0c.328 0 .651-.091.94-.266A2.1 2.1 0 0 0 7.66 11h.681a2.1 2.1 0 0 0 .718.734c.29.175.613.266.942.266.328 0 .651-.091.94-.266.29-.174.537-.427.719-.734h.681a2.1 2.1 0 0 0 .719.734c.289.175.612.266.94.266.329 0 .652-.091.942-.266.29-.174.536-.427.718-.734h.681c.183.307.43.56.719.734.29.174.613.266.941.266a1.819 1.819 0 0 0 1.06-.351M6 12a1.766 1.766 0 0 1-1.163-.476M5 12v7a1 1 0 0 0 1 1h2v-5h3v5h7a1 1 0 0 0 1-1v-7m-5 3v2h2v-2h-2Z"/>
                                 </svg>
