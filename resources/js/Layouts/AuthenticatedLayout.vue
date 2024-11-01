9010b519e3aa517334c62b3e0f5ffff4aae4380f
Niiikkid <niiikkid@gmail.com>
2024-11-02 03:32:47 +1000

    fix

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -2,7 +2,7 @@
 import {usePage, router, Link} from '@inertiajs/vue3';
 import {onMounted, ref} from 'vue'
 import { initFlowbite } from 'flowbite'
-import MenuModeSwitcher from "@/Layouts/Partials/MenuModeSwitcher.vue";
+import ViewModeSwitcher from "@/Layouts/Partials/ViewModeSwitcher.vue";
 import TraderMenu from "@/Layouts/Partials/TraderMenu.vue";
 import AdminMenu from "@/Layouts/Partials/AdminMenu.vue";
 import NavBar from "@/Layouts/Partials/NavBar.vue";
@@ -57,7 +57,7 @@ const openDocs = () => {
 
             <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
                 <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-                    <MenuModeSwitcher
+                    <ViewModeSwitcher
                         v-if="userStore.isAdmin"
                     />
                     <TraderMenu
--- resources/js/Layouts/Partials/ViewModeSwitcher.vue
+++ resources/js/Layouts/Partials/ViewModeSwitcher.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Select from "@/Components/Select.vue";
+
 import {useViewStore} from "@/store/view.js";
 
 const viewStore = useViewStore();
@@ -7,6 +8,7 @@ const viewStore = useViewStore();
 
 <template>
     <div class="mb-4">
+        <p class="text-sm text-gray-900 dark:text-gray-200 mb-1">Посмотреть сайт как</p>
         <select
             class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
