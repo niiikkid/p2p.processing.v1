1436b018129aee63a5f22004b84b45034a2ba3ec
Niiikkid <niiikkid@gmail.com>
2024-11-08 10:51:33 +1000

    payments

--- resources/js/Components/Select.vue
+++ resources/js/Components/Select.vue
@@ -35,7 +35,7 @@ defineProps({
 <template>
     <select
         :class="!error
-        ? 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        ? 'bg-white border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-700 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500'
         : 'bg-red-50 border border-red-500 text-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-gray-900 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'"
         required
         v-model="model"
