085200d105d415e040c22355971ee42834d1064e
Niiikkid <niiikkid@gmail.com>
2024-11-18 17:49:09 +1000

    fix

--- resources/js/Pages/PaymentLink/Index.vue
+++ resources/js/Pages/PaymentLink/Index.vue
@@ -580,13 +580,13 @@ defineOptions({ layout: PaymentLayout });
                 </button>
             </div>
 
-            <div class="text-xs text-black dark:text-white flex gap-2 cursor-pointer">
+<!--            <div class="text-xs text-black dark:text-white flex gap-2 cursor-pointer">
                 <div @click="stage = 'payment'" :class="{'text-blue-500' : stage === 'payment'}">payment</div>
                 <div @click="stage = 'success'" :class="{'text-blue-500' : stage === 'success'}">success</div>
                 <div @click="stage = 'fail'" :class="{'text-blue-500' : stage === 'fail'}">fail</div>
                 <div @click="stage = 'dispute_review'" :class="{'text-blue-500' : stage === 'dispute_review'}">dispute_review</div>
                 <div @click="stage = 'dispute_canceled'" :class="{'text-blue-500' : stage === 'dispute_canceled'}">dispute_canceled</div>
-            </div>
+            </div>-->
         </div>
     </div>
 </template>
