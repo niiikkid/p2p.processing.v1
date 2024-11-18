ad38b0f3b92b07bf47b1ca0be33c74144c897a82
Niiikkid <niiikkid@gmail.com>
2024-11-18 19:45:41 +1000

    ui

--- resources/js/Pages/Order/Index.vue
+++ resources/js/Pages/Order/Index.vue
@@ -14,6 +14,7 @@ import HeadlessTableTh from "@/Components/HeadlesTable/HeadlessTableTh.vue";
 import HeadlessTableTd from "@/Components/HeadlesTable/HeadlessTableTd.vue";
 import DateTime from "@/Components/DateTime.vue";
 import {useViewStore} from "@/store/view.js";
+import ShowAction from "@/Components/Table/ShowAction.vue";
 
 const viewStore = useViewStore();
 const orders = usePage().props.orders;
@@ -31,7 +32,7 @@ defineOptions({ layout: AuthenticatedLayout })
             :data="orders"
         >
             <template v-slot:body>
-                <div class="relative overflow-x-auto">
+<!--                <div class="relative overflow-x-auto">
                     <HeadllesTable>
                         <HeadlessTableTr
                             v-for="order in orders.data"
@@ -64,7 +65,69 @@ defineOptions({ layout: AuthenticatedLayout })
                             </HeadlessTableTd>
                         </HeadlessTableTr>
                     </HeadllesTable>
+                </div>-->
+
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                ID
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Сумма
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Реквизит
+                            </th>
+                            <th scope="col" class="px-6 py-3" v-if="viewStore.isAdminViewMode">
+                                Трейдер
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Статус
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Создан
+                            </th>
+                            <th scope="col" class="px-6 py-3 flex justify-center">
+                                <span class="sr-only">Действия</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr v-for="order in orders.data" class="bg-white border-b last:border-none dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-3 font-medium whitespace-nowrap text-gray-900 dark:text-gray-200">
+                                {{ order.id }}
+                            </th>
+                            <td class="px-6 py-3">
+                                <div class="text-nowrap">{{ order.amount }} {{ order.currency.toUpperCase() }}</div>
+                                <div class="text-nowrap text-xs">{{ order.profit }} {{ order.base_currency.toUpperCase() }}</div>
+                            </td>
+                            <td class="px-6 py-3">
+                                <PaymentDetail
+                                    :detail="order.payment_detail"
+                                    :type="order.payment_detail_type"
+                                    :copyable="false"
+                                ></PaymentDetail>
+                                <div class="text-xs">{{ order.payment_detail_name }}</div>
+                            </td>
+                            <td class="px-6 py-3" v-if="viewStore.isAdminViewMode">
+                                {{ order.user.email }}
+                            </td>
+                            <td class="px-6 py-3">
+                                <OrderStatus :status="order.status" :status_name="order.status_name"></OrderStatus>
+                            </td>
+                            <td class="px-6 py-3">
+                                <DateTime class="justify-center" :data="order.created_at"/>
+                            </td>
+                            <td class="px-6 py-3 text-right">
+                                <ShowAction link="#" @click.prevent="modalStore.openOrderModal({order})"></ShowAction>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
+
             </template>
         </MainTableSection>
 
