85945fe29227531c7ca9b969f7e819a441e6934d
Niiikkid <niiikkid@gmail.com>
2024-12-24 10:30:44 +1000

    sms filtres

--- app/Http/Controllers/Admin/SmsLogController.php
+++ app/Http/Controllers/Admin/SmsLogController.php
@@ -5,19 +5,62 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SmsLogResource;
 use App\Models\SmsLog;
+use App\Models\User;
+use Carbon\Carbon;
 use Inertia\Inertia;
 
 class SmsLogController extends Controller
 {
     public function index()
     {
+        $startDate = request()->input('filters.start_date');
+        if ($startDate) {
+            $startDate = Carbon::createFromFormat('d/m/Y', $startDate);
+        }
+
+        $endDate = request()->input('filters.end_date');
+        if ($endDate) {
+            $endDate = Carbon::createFromFormat('d/m/Y', $endDate);
+        }
+
+        if ($startDate && $endDate?->lessThan($startDate)) {
+            $endDate = null;
+        }
+
+        $user = request()->input('filters.user');
+
+        $currentFilters = [
+            'user' => $user,
+            'startDate' => $startDate?->format('d/m/Y'),
+            'endDate' => $endDate?->format('d/m/Y'),
+        ];
+
+        $users = User::query()
+            ->whereHas('smsLogs')
+            ->get()
+            ->transform(function ($user) {
+                return [
+                    'id' => $user->id,
+                    'email' => $user->email,
+                ];
+            });
+
         $sms_logs = SmsLog::query()
             ->with('user')
+            ->when($user, function ($query) use ($user) {
+                $query->where('user_id', $user);
+            })
+            ->when($startDate, function ($query) use ($startDate) {
+                $query->whereDate('created_at', '>=', $startDate);
+            })
+            ->when($endDate, function ($query) use ($endDate) {
+                $query->whereDate('created_at', '<=', $endDate);
+            })
             ->orderByDesc('id')
             ->paginate(10);
 
         $sms_logs = SmsLogResource::collection($sms_logs);
 
-        return Inertia::render('SmsLog/Index', compact('sms_logs'));
+        return Inertia::render('SmsLog/Index', compact('sms_logs', 'currentFilters', 'users'));
     }
 }
--- resources/js/Pages/SmsLog/Index.vue
+++ resources/js/Pages/SmsLog/Index.vue
@@ -1,12 +1,70 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import {Head, router} from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { usePage } from '@inertiajs/vue3';
 import MainTableSection from "@/Wrappers/MainTableSection.vue";
 import {useViewStore} from "@/store/view.js";
+import {computed, onMounted, ref} from "vue";
+import {Datepicker} from "flowbite-datepicker";
+import Select from "@/Components/Select.vue";
 
 const viewStore = useViewStore();
 const sms_logs = usePage().props.sms_logs;
+
+const users = ref(usePage().props.users);
+const currentFilters = ref(usePage().props.currentFilters);
+
+onMounted(() => {
+    const startDateDatepickerElement = document.getElementById('start-date-datepicker')
+    const endDateDatepickerElement = document.getElementById('end-date-datepicker')
+
+    startDateDatepickerElement.addEventListener('changeDate', (e) => {
+        currentFilters.value.startDate = e.target.value;
+    });
+    endDateDatepickerElement.addEventListener('changeDate', (e) => {
+        currentFilters.value.endDate = e.target.value;
+    });
+
+    Datepicker.locales.ru = {
+        days: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+        daysShort: ["Вск", "Пнд", "Втр", "Срд", "Чтв", "Птн", "Суб"],
+        daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        months: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+        monthsShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+        today: "Сегодня",
+        clear: "Очистить",
+        format: "dd.mm.yyyy",
+        weekStart: 1,
+        monthsTitle: 'Месяцы'
+    };
+
+    new Datepicker(startDateDatepickerElement, {
+        language: 'ru',
+        format: 'dd/mm/yyyy',
+    })
+    new Datepicker(endDateDatepickerElement, {
+        language: 'ru',
+        format: 'dd/mm/yyyy',
+    })
+})
+
+const filters = computed(() => {
+    return {
+        user: currentFilters.value.user,
+        start_date: currentFilters.value.startDate,
+        end_date: currentFilters.value.endDate,
+    }
+})
+
+const applyFilters = () => {
+    router.visit(route(route().current()), {
+        data: {
+            filters: filters.value,
+            page: 1
+        },
+        preserveScroll: true
+    })
+}
 
 defineOptions({ layout: AuthenticatedLayout })
 </script>
@@ -18,7 +76,72 @@ defineOptions({ layout: AuthenticatedLayout })
         <MainTableSection
             title="Сообщения"
             :data="sms_logs"
+            :query-date="{filters}"
         >
+            <template v-slot:table-header>
+                <section class="bg-gray-50 dark:bg-gray-900 flex items-center mb-5">
+                    <div class="mx-auto w-full">
+                        <div class="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                            <div class="flex flex-col xl:items-center justify-between p-4 space-y-3 lg:flex-row lg:space-y-0 lg:space-x-4">
+                                <div class="xl:flex items-center gap-4 xl:space-y-0 space-y-3">
+                                    <div class="flex items-center w-full space-x-3 lg:w-auto">
+                                        <select
+                                            class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            v-model="currentFilters.user"
+                                            @change="visitDefaultPage"
+                                        >
+                                            <option :value="null">Трейдер</option>
+                                            <option v-for="user in users" :value="user.id">{{ user.email }}</option>
+                                        </select>
+                                    </div>
+                                    <div class="flex items-center gap-2">
+                                        <div class="relative lg:max-w-sm w-full">
+                                            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                                </svg>
+                                            </div>
+                                            <input
+                                                datepicker
+                                                id="start-date-datepicker"
+                                                type="text"
+                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                placeholder="Начальная дата"
+                                                :value="currentFilters.startDate"
+                                            >
+                                        </div>
+                                        <span class="hidden lg:block mx-4 text-gray-500">до</span>
+                                        <div class="relative lg:max-w-sm w-full">
+                                            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                                </svg>
+                                            </div>
+                                            <input
+                                                datepicker
+                                                id="end-date-datepicker"
+                                                type="text"
+                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                placeholder="Конечная дата"
+                                                :value="currentFilters.endDate"
+                                            >
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex items-center">
+                                    <button
+                                        type="button"
+                                        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 h-[38px] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                        @click.prevent="applyFilters"
+                                    >
+                                        Применить
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </template>
             <template v-slot:body>
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -40,7 +163,7 @@ defineOptions({ layout: AuthenticatedLayout })
                                     Трейдер
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    Время
+                                    Создан
                                 </th>
                             </tr>
                         </thead>
@@ -62,7 +185,7 @@ defineOptions({ layout: AuthenticatedLayout })
                                     {{ sms_log.user.email }}
                                 </td>
                                 <td class="px-6 py-3 text-nowrap">
-                                    {{ sms_log.timestamp }}
+                                    {{ sms_log.created_at }}
                                 </td>
                             </tr>
                         </tbody>
