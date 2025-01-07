4295897215dba99781c419f78b83337d0fa82c77
Niiikkid <niiikkid@gmail.com>
2025-01-07 21:14:28 +1000

    fixes

--- resources/js/Pages/Merchant/Partials/Settings.vue
+++ resources/js/Pages/Merchant/Partials/Settings.vue
@@ -262,7 +262,7 @@ onMounted(() => {
                                             <svg class="w-3 h-3 mr-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 12c.263 0 .524-.06.767-.175a2 2 0 0 0 .65-.491c.186-.21.333-.46.433-.734.1-.274.15-.568.15-.864a2.4 2.4 0 0 0 .586 1.591c.375.422.884.659 1.414.659.53 0 1.04-.237 1.414-.659A2.4 2.4 0 0 0 12 9.736a2.4 2.4 0 0 0 .586 1.591c.375.422.884.659 1.414.659.53 0 1.04-.237 1.414-.659A2.4 2.4 0 0 0 16 9.736c0 .295.052.588.152.861s.248.521.434.73a2 2 0 0 0 .649.488 1.809 1.809 0 0 0 1.53 0 2.03 2.03 0 0 0 .65-.488c.185-.209.332-.457.433-.73.1-.273.152-.566.152-.861 0-.974-1.108-3.85-1.618-5.121A.983.983 0 0 0 17.466 4H6.456a.986.986 0 0 0-.93.645C5.045 5.962 4 8.905 4 9.736c.023.59.241 1.148.611 1.567.37.418.865.667 1.389.697Zm0 0c.328 0 .651-.091.94-.266A2.1 2.1 0 0 0 7.66 11h.681a2.1 2.1 0 0 0 .718.734c.29.175.613.266.942.266.328 0 .651-.091.94-.266.29-.174.537-.427.719-.734h.681a2.1 2.1 0 0 0 .719.734c.289.175.612.266.94.266.329 0 .652-.091.942-.266.29-.174.536-.427.718-.734h.681c.183.307.43.56.719.734.29.174.613.266.941.266a1.819 1.819 0 0 0 1.06-.351M6 12a1.766 1.766 0 0 1-1.163-.476M5 12v7a1 1 0 0 0 1 1h2v-5h3v5h7a1 1 0 0 0 1-1v-7m-5 3v2h2v-2h-2Z"/>
                                             </svg>
-                                            {{commissionSettings[gateway.id]['merchant_commission']}}%
+                                            {{parseFloat(commissionSettings[gateway.id]['merchant_commission']).toFixed(1)}}%
                                         </div>
                                         <div class="flex items-center text-gray-500 dark:text-gray-400">
                                             <svg class="w-3 h-3 mr-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -271,7 +271,7 @@ onMounted(() => {
                                             <template v-if="commissionSettings[gateway.id]['gateway_total_commission'] > 0 || commissionSettings[gateway.id]['gateway_total_commission'] === 0">
                                                 {{parseFloat(commissionSettings[gateway.id]['gateway_total_commission'] - commissionSettings[gateway.id]['merchant_commission']).toFixed(1)}}%
                                             </template>
-                                            <template>
+                                            <template v-else>
                                                 {{parseFloat(gateway.service_commission_rate - commissionSettings[gateway.id]['merchant_commission']).toFixed(1)}}%
                                             </template>
                                         </div>
